6422de119ae0e8dba6b76776c4e99a80cbe1b98d
alliewalker <walkera71193@gmail.com>
2019-06-02 19:56:42 -0700

    added smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,25 +1,42 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-  
-      // Add a click event on each of them
-      $navbarBurgers.forEach( el => {
-        el.addEventListener('click', () => {
-  
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
-  
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-  
-        });
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener("click", () => {
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
       });
-    }
-  
-  });
+    });
+  }
+});
+
+// ===== Scroll to Top ====
+$(window).scroll(function() {
+  if ($(this).scrollTop() >= 50) {
+    // If page is scrolled more than 50px
+    $("#return-to-top").fadeIn(200); // Fade in the arrow
+  } else {
+    $("#return-to-top").fadeOut(200); // Else fade out the arrow
+  }
+});
+$("#return-to-top").click(function() {
+  // When arrow is clicked
+  $("body,html").animate(
+    {
+      scrollTop: 0 // Scroll to top of body
+    },
+    500
+  );
+});
